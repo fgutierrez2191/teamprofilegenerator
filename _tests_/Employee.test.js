936bdb9cd7c8a1af51920362d9de22225c216b63
Felix Gutierrez <fgutierrez2191@icloud.com>
2020-11-30 18:21:20 -0500

    Employee.js functions finished

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,10 +1,2 @@
 const Employee = require('../lib/Employee');
 
-test('creates a employee object', () => {
-    const player = new Employee('Felix');
-
-    expect(player.name).toBe('Felix');
-    expect(player.id).toEqual(expect.any(""));
-    expect(player.email).toEqual(expect.any(""));
-    
-});
