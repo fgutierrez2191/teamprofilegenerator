72b5dbf7fa75142ff7e25169504f8dada3ceb6c7
Felix Gutierrez <fgutierrez2191@icloud.com>
2020-12-08 18:18:09 -0500

    cards work

--- src/generateProfile.js
+++ src/generateProfile.js
@@ -4,18 +4,61 @@
 const generateProfile = (team) => {
     const genManager = (manager) => {
         return `
-        <h2>${manager.getName()}</h2>`
+        <div class="card">
+        <div class="card-head">
+            <h3>${manager.getName()}</h3>
+            <h4>Role: Manager</h4>
+        </div>
+        <div class="card-body">
+            <ul>
+                <li>ID: ${manager.getEmail()}</li>
+                <li>Email: <a href="mailto: ${manager.getId()}" target="none">${manager.getId()}</a></li>
+                <li>Office Number: ${manager.getOfficeNumber()}</li>
+            </ul>
+        </div>
+    </div>
+`
+        
     }
 
     const genEngineer = (engineer) => {
         return `
-        <h2>${engineer.getName()}</h2>`
+        <div class="card">
+        <div class="card-head">
+            <h3>${engineer.getName()}</h3>
+            <h4>Role: Engineer</h4>
+        </div>
+        <div class="card-body">
+            <ul>
+                <li>ID: ${engineer.getEmail()}</li>
+                <li>Email: <a href="mailto: ${engineer.getId()}" target="none">${engineer.getId()}</a></li>
+
+            </ul>
+        </div>
+    </div>
+    `
 
     }
 
     const genIntern = (intern) => {
         return `
-        <h2>${intern.getName()}</h2>`
+        <div class="card">
+        <div class="card-head">
+            <h3>${intern.getName()}</h3>
+            <h4>Role: Intern</h4>
+        </div>
+        <div class="card-body">
+            <ul>
+                <li>ID: ${intern.getEmail()}</li>
+                <li>Email: <a href="mailto: ${intern.getId()}" target="none">${intern.getId()}</a></li>
+                <li>School:${intern.getSchool}</li>
+
+            </ul>
+        </div>
+    </div>
+        `
+
+    
     }
 
     const html = [];
@@ -31,38 +74,38 @@ const generateProfile = (team) => {
 module.exports = team => {
     return `
     <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>My Team</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/c502137733.js"></script>
-</head>
-<body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
-                <h1 class="text-center">My Team</h1>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>My Team</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 jumbotron mb-3 team-heading">
+                    <h1 class="text-center">My Team</h1>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="team-area col-12 d-flex justify-content-center">
-                ${generateProfile(team)}
+        <div class="container">
+            <div class="row">
+                <div class="team-area col-12 d-flex justify-content-center">
+                    ${generateProfile(team)}
+                </div>
             </div>
         </div>
-    </div>
-</body>
-</html>
+    </body>
+    </html>
+       
    
     `
 }
 
 
 
- // module.exports = generateProfile;
