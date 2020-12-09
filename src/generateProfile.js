8602bfd69133988f945e861712506bd1cc7619b8
Felix Gutierrez <fgutierrez2191@icloud.com>
2020-12-09 15:52:01 -0500

    bootstrap header

--- src/generateProfile.js
+++ src/generateProfile.js
@@ -82,7 +82,7 @@ module.exports = team => {
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <title>My Team</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
         <script src="https://kit.fontawesome.com/c502137733.js"></script>
     </head>
@@ -90,7 +90,7 @@ module.exports = team => {
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12 jumbotron mb-3 team-heading">
-                    <h1 class="text-center">My Team</h1>
+                    <h1 class="text-center">The Team!</h1>
                 </div>
             </div>
         </div>
